04b1e25d9d8e87108543ee691b7c49c20393f197
season <710205009@qq.com>
2018-05-12 00:02:02 +0800

    a~

--- learn-dubbo-consumer/src/main/java/com/season/controller/LoginController.java
+++ learn-dubbo-consumer/src/main/java/com/season/controller/LoginController.java
@@ -16,6 +16,7 @@ import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Map;
 
@@ -33,7 +34,7 @@ public class LoginController {
         return BaseResult.unKnow("未登录");
     }
 
-    @ApiOperation("首页")
+    @ApiIgnore
     @RequestMapping(value = "/index")
     public Object index(){
         return new BaseResult(1,"登录成功");
@@ -48,16 +49,17 @@ public class LoginController {
 
     //post登录
     @ApiOperation(value = "登录",httpMethod = "POST",notes = "通过shiro安全登录")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "username",required = true,dataType = "String",paramType = "String",value = "用户名"),
-            @ApiImplicitParam(name = "password",required = true,dataType = "String",paramType = "String",value = "密码")
-    })
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "username",required = true,dataType = "String",paramType = "String",value = "用户名"),
+//            @ApiImplicitParam(name = "password",required = true,dataType = "String",paramType = "String",value = "密码")
+//    })
+    @ApiParam()
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(@RequestBody Map<String,String> map){
+    public String login(@RequestParam("username") String userName, @RequestParam("password")String pwd){
         //添加用户认证信息
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken usernamePasswordToken
-                = new UsernamePasswordToken(map.get("username"), map.get("password"));
+                = new UsernamePasswordToken(userName, pwd);
         //进行验证，这里可以捕获异常，然后返回对应信息
         try {
             subject.login(usernamePasswordToken);
@@ -72,6 +74,7 @@ public class LoginController {
     }
 
     //错误页面展示
+    @ApiIgnore
     @RequestMapping(value = "/error",method = RequestMethod.POST)
     public Object error(){
         return new BaseResult(-1,"操作失败");
@@ -79,10 +82,11 @@ public class LoginController {
 
     //数据初始化
     @ApiOperation(value = "添加user",httpMethod = "POST")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "username",value = "用户名",dataType="String"),
-            @ApiImplicitParam(name = "password",value = "密码",dataType="String")
-    })
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "username",value = "用户名",dataType="String"),
+//            @ApiImplicitParam(name = "password",value = "密码",dataType="String")
+//    })
+    @ApiImplicitParam(name = "map",value = "用户",example = "{\"username\":\"aaa\",\"password\":\"123\"}")
     @RequestMapping(value = "/addUser",method = RequestMethod.POST)
     public String addUser(@RequestBody Map<String,Object> map){
         User user = loginService.addUser(map);
@@ -90,10 +94,7 @@ public class LoginController {
     }
 
     //角色初始化
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "userId",value = "用户id",dataType="String"),
-            @ApiImplicitParam(name = "roleName",value = "角色名称",dataType="String")
-    })
+    @ApiImplicitParam(name = "map",value = "角色",example = "{\"userId\":\"1\",\"roleName\":\"admin\"}")
     @ApiOperation(value = "添加角色",httpMethod = "POST")
     @RequestMapping(value = "/addRole")
     public String addRole(@RequestBody Map<String,Object> map){

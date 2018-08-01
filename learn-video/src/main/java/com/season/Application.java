148b9e02cda99e3bd838a0b2280674166a752e53
season <710205009@qq.com>
2018-08-02 01:11:56 +0800

    新加video.js

---
+++ learn-video/src/main/java/com/season/Application.java
@@ -0,0 +1,16 @@
+package com.season;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by season on 2018/8/2.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
